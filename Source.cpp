cf7583bdcc1a65730aaafb6f7d3b1fa3f87021a7
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-05-27 14:01:41 +0100

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2;
+	cout << "Please enter your first number: ";
+	cin >> num1;
+	cout << "Please enter your second number: ";
+	cin >> num2;
+	cout << "Please select an operation from the following list" << endl << "1. Add" << endl << "2. Subtract" << endl << "3. Multiply" << endl << "4. Divide";
+}
