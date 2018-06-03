1b4d178dda8c94f2d4e7e76f90e11373a9ca36ea
PCandCG <37952236+PCandCG@users.noreply.github.com>
2018-06-03 10:54:38 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,26 @@ using namespace std;
 int main() {
 	int num1;
 	int num2;
-	cout << "Please enter your first number: ";
+	int operation;
+	float answer;
+	cout << "Please enter your first whole number: ";
 	cin >> num1;
-	cout << "Please enter your second number: ";
+	cout << "Please enter your second whole number: ";
 	cin >> num2;
-	cout << "Please select an operation from the following list" << endl << "1. Add" << endl << "2. Subtract" << endl << "3. Multiply" << endl << "4. Divide";
+	cout << "Please select an operation from the following list" << endl << "1. Add" << endl << "2. Subtract" << endl << "3. Multiply" << endl << "4. Divide" << endl;
+	cin >> operation;
+	if (operation == 1) {
+		answer = num1 + num2;
+	}
+	else if (operation == 2) {
+		answer = num1 - num2;
+	}
+	else if (operation == 3) {
+		answer = num1 * num2;
+	}
+	else if (operation == 4) {
+		answer = num1 / num2;
+	}
+	cout << "Answer: " << answer;
+	return 0;
 }
